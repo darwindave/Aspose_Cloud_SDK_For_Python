ca8c1ce2c0d8088f43b8d3b894efe43282c1f10c
montage <dave@montagesoftware.com.au>
2015-03-02 14:25:17 +1100

    py3 bytearray str issues

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup, find_packages
 setup(
     name = 'asposecloud',
     packages = find_packages(),
-    version = '1.0.5',
+    version = '1.0.5.a',
     description = 'Aspose Cloud SDK for Python allows you to use Aspose API in your Python applications',
     author='Assad Mahmood Qazi',
     author_email='assadvirgo@gmail.com',
     url='https://github.com/asposeforcloud/Aspose_Cloud_SDK_For_Python',
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent'
     ]
